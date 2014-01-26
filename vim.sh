48c544bbe741b815efeee46dba3fa18201b4bfbd
hellojinjie <hellojinjie@gmail.com>
2014-01-26 18:03:59 +0800

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -4,7 +4,7 @@
 #
 # Easiest way to use this is to run this from your home folder in Terminal:
 #
-# curl https://raw.github.com/gist/532968/vim.sh | sh
+# curl https://raw.github.com/hellojinjie/vimrc/master/vim.sh | sh
 #
 # You'll need Vim, Git and Curl installed to use this script with bash.
 # 
