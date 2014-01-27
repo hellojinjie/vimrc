f60af949703c86c7728fa8635d58af2cacc32989
hellojinjie <hellojinjie@gmail.com>
2014-01-27 10:29:17 +0800

    replace git@ to https://

--- vim.sh
+++ vim.sh
@@ -15,24 +15,24 @@ mkdir -p .vim/bundles
  
 cd .vim/bundles
  
-git clone git://github.com/tpope/vim-rails.git
-git clone git://github.com/tpope/vim-cucumber.git
-git clone git://github.com/tpope/vim-haml.git
-git clone git://github.com/tpope/vim-endwise.git
-git clone git://github.com/scrooloose/nerdtree.git
-git clone git://github.com/scrooloose/nerdcommenter.git
-git clone git://github.com/tpope/vim-surround.git
-git clone git://github.com/tpope/vim-vividchalk.git
+git clone https://github.com/tpope/vim-rails.git
+git clone https://github.com/tpope/vim-cucumber.git
+git clone https://github.com/tpope/vim-haml.git
+git clone https://github.com/tpope/vim-endwise.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-vividchalk.git
 git clone https://github.com/tpope/vim-ragtag.git
-git clone git://github.com/vim-ruby/vim-ruby.git
-git clone git://github.com/tsaleh/vim-align.git
-git clone git://github.com/msanders/snipmate.vim.git
+git clone https://github.com/vim-ruby/vim-ruby.git
+git clone https://github.com/tsaleh/vim-align.git
+git clone https://github.com/msanders/snipmate.vim.git
 git clone https://github.com/geekq/ack.vim.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/vim-scripts/VimClojure.git
 git clone https://github.com/benmills/vimux.git
 git clone https://github.com/pgr0ss/vimux-ruby-test
-git clone git@github.com:derekwyatt/vim-scala.git
+git clone https://github.com/derekwyatt/vim-scala.git
 cd ../..
  
 # Puts Pathogen in the right location
