716c6d1deb8b83a22280e479fe8c047d9e362689
hellojinjie <hellojinjie@gmail.com>
2015-02-20 20:05:26 +0800

    follow rediretion in curl

--- vim.sh
+++ vim.sh
@@ -4,7 +4,7 @@
 #
 # Easiest way to use this is to run this from your home folder in Terminal:
 #
-# curl https://raw.github.com/hellojinjie/vimrc/master/vim.sh | sh
+# curl -L https://raw.github.com/hellojinjie/vimrc/master/vim.sh | sh
 #
 # You'll need Vim, Git and Curl installed to use this script with bash.
 # 
@@ -36,7 +36,7 @@ git clone https://github.com/derekwyatt/vim-scala.git
 cd ../..
  
 # Puts Pathogen in the right location
-curl https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim > .vim/autoload/pathogen.vim
+curl -L https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim > .vim/autoload/pathogen.vim
  
-curl https://raw.github.com/hellojinjie/vimrc/master/vimrc > .vimrc   
-curl https://raw.github.com/hellojinjie/vimrc/master/vimrc_main > .vimrc_main
+curl -L https://raw.github.com/hellojinjie/vimrc/master/vimrc > .vimrc   
+curl -L https://raw.github.com/hellojinjie/vimrc/master/vimrc_main > .vimrc_main
